f0816e3102cf88de0f98d47f90f9602fb396b59f
PrashantWebGL <prashant.dh12@gmail.com>
2024-02-20 02:17:25 +0900

    Added grid for experience page

---
+++ src/app/components/companycard.js
@@ -0,0 +1,37 @@
+import "@/app/components/companycard.css"
+
+export default async function CompanyCard() {
+    const list = [
+        {
+            title: "Wondevari"
+        },
+        {
+            title: "Dassault Systems"
+        },
+        {
+            title: "Siemens"
+        },
+        {
+            title: "ProtoTech Soltions"
+        },
+        {
+            title: "CDAC"
+        },
+        {
+            title: "Personal Projects"
+        }
+    ];
+
+    //let data =  props.props;
+    return (
+        <div className="companycard-parent">
+            {
+                list.map((item, index) => (
+
+                    <section className="companycard"><h1>{item.title}</h1></section>
+
+                ))
+            }
+        </div>
+    )
+}
--- src/app/experience/page.js
+++ src/app/experience/page.js
@@ -1,16 +1,11 @@
+import CompanyCard from "../components/companycard";
 
 export default function ExperienceComp(){
-    return(
         
-            
-            <article>
-                    <h1>This is an Experience Component</h1>
-                    <p>This is an Experience Component. This is an Experience Component.This is an Experience Component.This is an Experience Component.vvThis is an Experience Component. This is an Experience Component.
-                    This is an Experience Component
-                    This is an Experience Component
-                    This is an Experience Component
-                    </p>
-            </article>
+    return(  
+            <div>
+                <CompanyCard />
+            </div>
         
     )
 }
