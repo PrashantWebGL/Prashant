003b23f1e146f7a60e3eb03030a73dbc176ff1eb
PrashantWebGL <prashant.dh12@gmail.com>
2024-02-21 10:42:13 +0900

    Work in progress for incomplete pages

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,16 +1,19 @@
+import Image from "next/image"
+export default function AboutComp() {
+        return (
 
-export default function AboutComp(){
-    return(
-        
-            
-            <article>
-                    <h1>This is an About Component</h1>
-                    <p>This is an About Component. This is an About Component.This is an About Component.This is an About Component.vvThis is an About Component. This is an About Component.
-                    This is an About Component
-                    This is an About Component
-                    This is an About Component
-                    </p>
-            </article>
-        
-    )
+
+                <article>
+                        <h1>Wrok in progress !! </h1>
+                        <Image src={"/WIP.png"} alt={"Wip.png file"} height={300} width={300} ></Image>
+                        <h1>This is an About Component</h1>
+                        <p>This is an About Component. This is an About Component.This is an About Component.This is an About Component.vvThis is an About Component. This is an About Component.
+                                This is an About Component
+                                This is an About Component
+                                This is an About Component
+                        </p>
+
+                </article>
+
+        )
 }
--- src/app/experience/[title]/page.js
+++ src/app/experience/[title]/page.js
@@ -1,8 +1,10 @@
+import Image from "next/image";
 
 export default function CompanyInfo(){
     return(
         <section>
-            <h1>This is indvidual Information about Shweta!! </h1>
+            <h1>Wrok in progress !! </h1>
+            <Image src={"/WIP.png"} alt={"Wip.png file"} height={300} width={300} ></Image>
         </section>
     )
 }
