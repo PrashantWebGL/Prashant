6599f6f8f098454ab8a7ee0993465f2978a713c6
PrashantWebGL <prashant.dh12@gmail.com>
2025-09-03 03:41:14 +0900

    Changed map and location

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -15,17 +15,16 @@ export default function AboutComp() {
                         <div className={styles.about_info_text}>
                                 <h1> About Me, </h1>
                                 <h3>If you guessed it right! Yes, The birds flying in home page at the background are 3D birds and they are rendereed using three.js</h3>
-                                <h3>I never stop bragging about how cool 3D model rendering is! It lets you think in three dimention, it is challenging and you need to control camera, lights,action just like a Movie </h3>
+                                <h3>I never stop bragging about how cool 3D model rendering is! It lets you think in three dimensional, it is challenging and you need to control camera, lights,action just like a Movie </h3>
                                 <h3>When I am not coding at home, you might find me cooking some amazing dishes in kitchen. I personally believe adding artistic personal touch is so satisfying while exhibiting work.
                                         <Image src={"/cooking.gif"} alt={"This is about gif"} width={40} height={30}></Image></h3>
                                 <h3>Like most of the Indians, I love cricket.
                                         <Image src={"/cricket1.gif"} alt={"This is about gif"} width={40} height={30}></Image></h3>
 
-                                <h3>I am currently based in Seoul, South Korea. Do contact me !! </h3>
+                                <h3>I am currently based in Pune, India. Do contact me !! </h3>
                                 
                                 <h3><a href="mailto:prashant.dh.12@gmail.com" target="_blank">  <MdEmail /> prashant.dh.12@gmail.com </a></h3>
                                 <h3><FaWhatsapp /> +91 90 9695 7263</h3>
-                                <h3><FaPhone /> +82 10 8309 7263</h3>
 
                                 <Link href="https://www.linkedin.com/in/prashantdh/" target="_blank" >
                                         <Image src={"/linkedin.png"} alt={"This is about gif"} width={32} height={32} className={styles.about_logo}></Image>
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -15,7 +15,7 @@ const Contact = () => {
             </div>
 
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d789.75233987346!2d127.061258!3d37.648984!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cb94286f6a50d%3A0x24db9a20b4cf1f9a!2zU291dGggS29yZWEsIFNlb3VsLCBTYW5nZ3llLWRvbmcsIDIwNyDsg4Hqs4Tso7zqs7Uy64uo7KeA7JWE7YyM7Yq4!5e0!3m2!1sen!2sus!4v1708602222950!5m2!1sen!2sus"
+                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d472.66974039338925!2d73.74422013748234!3d18.6029642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTjCsDM2JzEwLjUiTiA3M8KwNDQnNDEuMiJF!5e0!3m2!1sen!2sin!4v1756838043916!5m2!1sen!2sin"
                 width={100} height={450} style={{ border: 0 }} allowFullScreen="" loading="lazy" className={styles.mapping}
                 referrerPolicy="no-referrer-when-downgrade">
 
